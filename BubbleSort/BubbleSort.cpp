566f38cc5d88599f92d2e24d4fce18a521074ae5
jkarayel <jiyankarayel@gmail.com>
2023-10-10 10:25:27 -0500

    fırst ınıt

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -1,20 +1,34 @@
-// BubbleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-
+void BubbleSort(int array[], int size);
+void displaying(int array[], int size);
 int main()
 {
-    std::cout << "Hello World!\n";
+	int numbers[] = { 13, 6, 89, 90, 2, 5, 23, 46, 88, 100, 7 };
+	int size = sizeof(numbers) / sizeof(numbers[0]);
+	BubbleSort(numbers, size);
+	displaying(numbers, size);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void displaying(int array[], int size) {
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << array[i] << "\t";
+	}
+}
+void BubbleSort(int array[], int size) {
+	for (int i = 0; i < size - 1; i++)
+	{
+		int temp;
+		for (int j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j+1])
+			{
+				temp = array[j];	
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}	
+		}
+		
+	}
+}
